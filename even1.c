60b06ec342a82747c94bb155dd07b7cbf026a090
SaswatMantri <saswatmantri@gmail.com>
2021-03-24 19:20:17 +0530

    Even1

---
+++ even1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+int n=0;
+printf("input a number	");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("the number is even");
+
+}
+else
+{
+printf("the no is odd");
+}
+//return 0;
+}
